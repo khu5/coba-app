4dc75b6cc56b7e547ea324553a7e22fde2cc7226
Khusnul Karomah <92246747+khu5@users.noreply.github.com>
2024-11-01 10:18:47 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly as px
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+
+scatter_column, setting_column = st.beta_column((4, 1))
+
+scatter_column.title("Multi-Dimensional Analysis")
+
+setting_column.title("Settings")
